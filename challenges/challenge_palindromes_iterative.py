ca605e2eafe8f79ca88f88f22d74207c001dee69
PatriciaDivi <patriciavianad@gmail.com>
2022-11-18 19:38:56 -0300

    feat(Development): :sparkles: implementado função 'is_palindrome_iterative'
    Para a validação se a STRING passada é um palindrome(se é lida de trás para frente e de frente para trás)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,12 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+
+    palindrome = False
+    pos = 0
+    meio = len(word) // 2
+    while pos <= meio:
+        palindrome = word[pos] == word[-pos-1]
+        pos += 1
+    return palindrome
+ 

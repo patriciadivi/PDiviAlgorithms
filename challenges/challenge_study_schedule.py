bc7968220505860196100f4381a961a5431f34a7
PatriciaDivi <patriciavianad@gmail.com>
2022-11-21 19:19:15 -0300

    feat(Development): :sparkles: Implementado função 'study_schedule'
    Retorne None se em permanence_period houver alguma entrada inválida. Retorne None se target_time recebe um valor vazio
    
    A função deverá, por meio de análise empírica, se comportar (no avaliador remoto em sua Pull Request) como no máximo O(n), ou seja, com complexidade assintótica linear

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,19 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        posStudents = 0
+        pos = 0
+        while pos < len(permanence_period):
+            if (
+                permanence_period[pos][0]
+                <= target_time <= permanence_period[pos][1]
+            ):
+                posStudents += 1
+            pos += 1
+        return posStudents
+    except TypeError:
+        return None
+
+
+# if __name__ == '__main__':
+#     print(study_schedule(
+# [(2, None), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), (6, 7)], 5))

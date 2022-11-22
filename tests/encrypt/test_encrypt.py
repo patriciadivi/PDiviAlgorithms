947c1801149c7ae2637cfc5e9ba90547e637d6f9
PatriciaDivi <patriciavianad@gmail.com>
2022-11-21 21:34:48 -0300

    feat(Development): :sparkles: implementado função 'test_encrypt_message'
    Para teste 'ordem invertida' | 'Validar Par' após underscore(_) | 'Validar impar antes do underscore(_)
    
    'Validar se as messagens de erros são capituradas'

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,21 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    name = 'patricia'
+    numbers = [40, 2, 3, 1]
+    validateInverter = encrypt_message(name, numbers[0])
+    assert (validateInverter) == "aicirtap"
+
+    validatePair = encrypt_message(name, numbers[1])
+    assert (validatePair) == "aicirt_ap"
+
+    validateOdd = encrypt_message(name, numbers[2])
+    assert (validateOdd) == "tap_aicir"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(name, "")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(numbers[3], numbers[1])

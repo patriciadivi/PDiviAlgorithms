702c4851f8c24d695733b2f22e31ab2ed779f5e1
PatriciaDivi <patriciavianad@gmail.com>
2022-11-22 19:34:57 -0300

    feat(Development): :sparkles: Implementado a função 'is_palindrome_recursive'
    Retorne 'True' se o parâmetro passada for um palíndromo; Retorne 'False' se o parâmetro passada não for um palíndromo;
    
    Retorne False se nenhuma palavra for passada por parâmetro.

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,29 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    palindrome = False
+    pos = 0
+    while pos < len(word):
+        if word == "":
+            return False
+        elif high_index <= low_index:
+            return True
+        else:
+            palindrome = (
+                word[low_index] == word[high_index]
+                and is_palindrome_recursive(
+                    word, low_index + 1, high_index - 1
+                )
+            )
+        pos += 1
+    return palindrome
+
+
+#  Segundo Jeito de fazer
+    # if word == "":
+    #     return False
+    # elif high_index <= low_index:
+    #     return True
+    # elif word[low_index] == word[high_index]:
+    #     initial_index = low_index + 1
+    #     final_index = high_index - 1
+    #     return is_palindrome_recursive(word, initial_index, final_index)
+    # return False

b54f6231153a9e22ca8e7118858b69ace4ec38c3
PatriciaDivi <patriciavianad@gmail.com>
2022-11-23 18:11:04 -0300

    fix(BUGS): :bug: fix bug

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,11 +1,11 @@
-def element_partition(arrayParams, initPos, endPos):
-    pivot = arrayParams[endPos]
+def element_partition(list, initPos, endPos):
+    pivot = list[endPos]
     index = initPos - 1
     for j in range(initPos, endPos):
-        if arrayParams[j] <= pivot:
+        if list[j] <= pivot:
             index += 1
-            arrayParams[index], arrayParams[j] = arrayParams[j], arrayParams[index]
-    arrayParams[index + 1], arrayParams[endPos] = arrayParams[endPos], arrayParams[index + 1]
+            list[index], list[j] = list[j], list[index]
+    list[index + 1], list[endPos] = list[endPos], list[index + 1]
     return index + 1
 
 
@@ -16,15 +16,14 @@ def order_elements(array, initPos, endPos):
         order_elements(array, pivot + 1, endPos)
     return array
 
+
 def is_anagram(first_string, second_string):
     firstString = list(first_string.lower())
     secondString = list(second_string.lower())
     resultFirstString = order_elements(firstString, 0, len(firstString) - 1)
     resultSecondString = order_elements(secondString, 0, len(secondString) - 1)
-    
     if resultFirstString == "" or resultSecondString == "":
         validateResult = False
-    
     validateResult = resultFirstString == resultSecondString
     return (
         "".join(resultFirstString),
